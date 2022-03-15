56e9a534193bcfd98a962846c00af52e61d34722
malmiteria <malmiteria@gmail.com>
2022-03-15 15:53:42 +0100

    removed scenarios folder, as everything is describe in tests now

--- scenarios/simple/test.py
+++
@@ -1,8 +0,0 @@
-
-def test_super():
-    from .super import A
-    assert list(A().method()) == ['A', 'B']
-
-def test_parent():
-    from .parent import A
-    assert list(A().method()) == ['A', 'B']
